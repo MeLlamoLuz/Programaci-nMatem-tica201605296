ee1303466b261b6c708623dbd371453c727ad83b
MeLlamoLuz <lucy.martinezxd@gmail.com>
2017-11-06 01:16:51 -0600

    Ejercicio 34 actualizado

--- Ejercicios/Ejercicio34.py
+++ Ejercicios/Ejercicio34.py
@@ -1,17 +1,21 @@
 def factorial(n):
-    if n == 0:
-        return 1
-    else:
-        return n*factorial(n-1)
+  if n < 2:
+    return 1
+  else:
+    result = 1
+    for i in xrange (2, n + 1):
+      result = result * i
+    return result
 
 def digitos(n):
     suma = 0
     for x in list(str(n)):
         suma += factorial(int(x))
+    return suma
         
 numeros = []
 
-for x in range():
+for x in xrange(10000001):
     if digitos(x) == x:
         numeros.append(x)
         
